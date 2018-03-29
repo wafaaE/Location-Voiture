acac1d6f6b9b1a7945b4d1543a0b4f53978846da
Clément Boudville <clement.boudville@gmail.com>
2018-03-29 12:16:07 +0200

    Init some files

---
+++ src/main/java/com/example/demo/Controllers/ReservationController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controllers;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entities.Reservation;
+
+@RestController
+public class ReservationController {
+	
+
+}
